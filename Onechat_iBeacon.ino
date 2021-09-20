0c1b7fdd8538f454d2f247b20fea872ad74d9b45
ฉัตรเพชร เคนานัน (CHATPETH KENANAN) <5610110106@email.psu.ac.th>
2021-09-20 16:02:46 +0700

    define value

--- Onechat_iBeacon.ino
+++ Onechat_iBeacon.ino
@@ -38,19 +38,27 @@ BLEAdvertising *pAdvertising;
 struct timeval now;
 
 #define BEACON_UUID           "b4b89731-4797-dc8a-3a48-d53c424fa9c8" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
-#define IP_B_UUID             "B4B897314797-DC8A-3A48-D53C-424FA9C8"
+#define MFID 0x4C00
+#define MAJOR 0x8002
+#define MINOR 0x03E8
+#define TX_POWER 0xC3
+#define APPLE_FLAG 0x04
+#define MIN_INTV 0x64
+#define MAX_INTV 0x100
+
 void setBeacon() {
 
   BLEBeacon oBeacon = BLEBeacon();
-  oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
+  oBeacon.setManufacturerId(MFID); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
   oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor(0x8004);
-  oBeacon.setMinor(0x02);
-  oBeacon.setSignalPower(0xC3);
+  oBeacon.setMajor(MAJOR);
+  oBeacon.setMinor(MINOR);
+  oBeacon.setSignalPower(TX_POWER);
+  
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
   BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
   
-  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
+  oAdvertisementData.setFlags(APPLE_FLAG); // BR_EDR_NOT_SUPPORTED 0x04
   
   std::string strServiceData = "";
   
@@ -62,8 +70,8 @@ void setBeacon() {
   pAdvertising->setAdvertisementData(oAdvertisementData);
   pAdvertising->setScanResponseData(oScanResponseData);
   pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND);
-  pAdvertising->setMinInterval(0x64);
-  pAdvertising->setMaxInterval(0x100);
+  pAdvertising->setMinInterval(MIN_INTV);
+  pAdvertising->setMaxInterval(MAX_INTV);
 }
 
 void setup() {
@@ -72,7 +80,7 @@ void setup() {
   Serial.begin(115200);
   
   // Create the BLE Device
-  BLEDevice::init("my-beacon");
+  BLEDevice::init("iBeacon");
 
   // Create the BLE Server
   // BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
